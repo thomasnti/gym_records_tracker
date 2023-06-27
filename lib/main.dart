70000ad35d62c0028c77702bb57b61233451e8df
Thomas Ntinas <86719110+thomasnti@users.noreply.github.com>
2023-06-27 16:34:35 +0300

    SETUP Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,29 @@
+import 'dart:ui';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 
 import 'common/bottom_tab_bar.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  // Synchronous errors
+  FlutterError.onError = (errorDetails) {
+    FirebaseCrashlytics.instance.log("Higgs-Boson detected! Bailing out");
+    FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
+  };
+  // Pass all uncaught asynchronous errors that aren't handled by the Flutter framework to Crashlytics
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.log("Higgs-Boson detected! Bailing out");
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
+
   runApp(const GymRecords());
 }
 
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,1 +1,7 @@
+import 'package:flutter_test/flutter_test.dart';
 
+void main() {
+  test('first test', () {
+    expect(1, 1);
+  });
+}
