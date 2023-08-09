3a63bbff8a629602038b61000b035bad82d475aa
Thomas Ntinas <86719110+thomasnti@users.noreply.github.com>
2023-08-09 18:59:55 +0300

    fix analysis, ci.yml

--- lib/features/workout/presentation/bloc/exercise/cubit/exercise_cubit.dart
+++ lib/features/workout/presentation/bloc/exercise/cubit/exercise_cubit.dart
@@ -1,5 +1,5 @@
-import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:injectable/injectable.dart';
 import 'package:mediatr/mediatr.dart';
 
