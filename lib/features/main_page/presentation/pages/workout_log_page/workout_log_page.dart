fdc9b24dd45305c5a2a092e92d32ac5bd2a7e48a
Thomas Ntinas <86719110+thomasnti@users.noreply.github.com>
2024-03-06 14:36:31 +0200

    Implement pull to refresh functionality #27

--- lib/features/main_page/presentation/pages/workout_log_page/workout_log_page.dart
+++ lib/features/main_page/presentation/pages/workout_log_page/workout_log_page.dart
@@ -27,8 +27,10 @@ class WorkoutLogPage extends StatelessWidget {
               );
             }
 
-            return Padding(
-              padding: const EdgeInsets.all(16.0),
+            return RefreshIndicator(
+              onRefresh: () async {
+                context.read<WorkoutLogBloc>().add(GetSavedWorkoutsEvent());
+              },
               child: ListView.separated(
                 // physics: const NeverScrollableScrollPhysics(),
                 // shrinkWrap: true,
@@ -39,93 +41,80 @@ class WorkoutLogPage extends StatelessWidget {
                 itemBuilder: (context, index) {
                   final workout = state.workouts[index];
 
-                  return Row(
-                    children: [
-                      Column(
-                        children: [
-                          Text(workout.workoutDateParts!.dayShort),
-                          Text(workout.workoutDateParts!.dayOfMonth),
-                          Text(workout.workoutDateParts!.monthShort),
-                        ],
-                      ),
-                      const SizedBox(width: 20),
-                      Expanded(
-                        child: Slidable(
-                          endActionPane: ActionPane(
-                            extentRatio: 0.3,
-                            motion: const ScrollMotion(),
-                            children: [
-                              SlidableAction(
-                                label: 'Delete',
-                                backgroundColor: Colors.red,
-                                icon: Icons.delete,
-                                onPressed: (ctx) => ctx
-                                    .read<WorkoutLogBloc>()
-                                    .add(DeleteWorkoutEvent(workout.workoutId)),
-                              )
-                            ],
-                          ),
-                          child: Stack(
-                            children: [
-                              Container(
-                                // alignment: Alignment.centerLeft,
-                                width: double.infinity, // takes the available space
-                                constraints: const BoxConstraints(minHeight: 60),
-                                decoration: BoxDecoration(
-                                  color: Colors.lightGreen.shade100,
-                                  borderRadius: BorderRadius.circular(10),
-                                ),
-                                child: Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: Column(
-                                    crossAxisAlignment: CrossAxisAlignment.start,
-
-                                    children: workout.exercises
-                                        .map(
-                                          (e) => Text(
-                                            '${e.exerciseSets.length}x  ${e.exerciseName}',
-                                          ),
-                                        )
-                                        .toList(),
-                                    // children: [
-                                    //   Text('2x Upright Rows'),
-                                    //   Text('2x Bicep curls'),
-                                    //   Text('2x Upright Rows'),
-                                    //   Text('2x Upright Rows'),
-                                    // ],
-                                  ),
-                                ),
-                              ),
-                              Positioned(
-                                top: 8, // same as padding
-                                right: 8,
-                                child: Text(
-                                  // '55 min',
-                                  _displayDuration(workout),
-                                  style: const TextStyle(
-                                    fontSize: 12,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
+                  return Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Row(
+                      children: [
+                        Column(
+                          children: [
+                            Text(workout.workoutDateParts!.dayShort),
+                            Text(workout.workoutDateParts!.dayOfMonth),
+                            Text(workout.workoutDateParts!.monthShort),
+                          ],
                         ),
-                      )
-                    ],
+                        const SizedBox(width: 20),
+                        Expanded(
+                          child: Slidable(
+                            endActionPane: ActionPane(
+                              extentRatio: 0.3,
+                              motion: const ScrollMotion(),
+                              children: [
+                                SlidableAction(
+                                  label: 'Delete',
+                                  backgroundColor: Colors.red,
+                                  icon: Icons.delete,
+                                  onPressed: (ctx) => ctx
+                                      .read<WorkoutLogBloc>()
+                                      .add(DeleteWorkoutEvent(workout.workoutId)),
+                                )
+                              ],
+                            ),
+                            child: Stack(
+                              children: [
+                                Container(
+                                  // alignment: Alignment.centerLeft,
+                                  width: double.infinity, // takes the available space
+                                  constraints: const BoxConstraints(minHeight: 60),
+                                  decoration: BoxDecoration(
+                                    color: Colors.lightGreen.shade100,
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: workout.exercises
+                                          .map(
+                                            (e) => Text(
+                                              '${e.exerciseSets.length}x  ${e.exerciseName}',
+                                            ),
+                                          )
+                                          .toList(),
+                                    ),
+                                  ),
+                                ),
+                                Positioned(
+                                  top: 8, // same as padding
+                                  right: 8,
+                                  child: Text(
+                                    // '55 min',
+                                    _displayDuration(workout),
+                                    style: const TextStyle(
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
                   );
-                  // return Column(
-                  //   children: [
-                  //     Text(state.workouts[index].workoutDate),
-                  //     const SizedBox(height: 20),
-                  //   ],
-                  // );
                 },
               ),
             );
-            // return const Center(
-            //   child: Text('here you will see previous workouts'),
-            // );
           },
         ),
       ),
